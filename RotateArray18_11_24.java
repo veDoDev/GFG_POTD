da6880b8acf01a9b42786a8c116cf1769f912af5
Ved_S_Tripathi <161162711+veDoDev@users.noreply.github.com>
2024-11-18 18:09:25 +0530

    Create RotateArray18_11_24.java
    gfg link : https://www.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1
    
    Given an unsorted array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
    
    Note: Consider the array as circular.
    
    Constraints:
    1 <= arr.size(), d <= 105
    0 <= arr[i] <= 105

---
+++ RotateArray18_11_24.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        d = d % arr.length;
+        int[] rotatedArray = new int[arr.length];
+        int index = 0;
+        
+        for(int i = d; i < arr.length; i++)
+        {
+            rotatedArray[index] = arr[i];
+            index++;
+        }
+        for(int i = 0; i < d; i++)
+        {
+            rotatedArray[index] = arr[i];
+            index++;
+        }
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = rotatedArray[i];
+        }
+    }
+} 
